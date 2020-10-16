433b5226a2e334694428b6775c34699fb4c28e9c
andy <andy.william.mcdonald@gmail.com>
2020-10-16 13:58:28 +0100

    part 3.10 in progress

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 morgan.token('data', (req, res) => {
@@ -109,7 +111,7 @@ app.post("/api/persons", (request, response) => {
   response.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
